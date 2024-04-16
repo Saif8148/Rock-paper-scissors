1b35c87aa4c9dc7ef8db60114eedbce73336f2ae
Saif8148 <147059332+Saif8148@users.noreply.github.com>
2024-04-16 17:48:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,70 +1,8 @@
-// function rock() 
-// {
-//    document.getElementById("you").style.background = "url('https://images.pexels.com/photos/161702/harmony-relax-rock-moqui-161702.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1') center/cover"
-//    let a = Math.floor(Math.random()*3)
-//    console.log(a)
-//    if (a==0)
-//    {
-//     document.getElementById("com").style.background = "url('https://images.pexels.com/photos/161702/harmony-relax-rock-moqui-161702.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1') center/cover"
-//     document.getElementById("result").innerHTML="Match Draw"
-//     document.getElementById("result").style.color="White"
-// }
-//     else if (a==1) {
-//         document.getElementById("com").style.background = "url('https://images.pexels.com/photos/268349/pexels-photo-268349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1https://cdn.pixabay.com/photo/2014/04/10/19/53/scissors-321238_960_720.jpg') center/cover"
-//         document.getElementById("result").innerHTML="Com Win"
-//     document.getElementById("result").style.color="red"
-
-//     }
-//     else if (a==2) {
-//         document.getElementById("com").style.background = "url('https://images.pexels.com/photos/4226910/pexels-photo-4226910.jpeg?auto=compress&cs=tinysrgb&w=1200') center/cover"
-//         document.getElementById("result").innerHTML="You Win"
-//     document.getElementById("result").style.color="green"
-
-
-//     }
-// }
-// function paper() 
-// {
-//    document.getElementById("you").style.background = "url('https://images.pexels.com/photos/268349/pexels-photo-268349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1https://cdn.pixabay.com/photo/2014/04/10/19/53/scissors-321238_960_720.jpg') center/cover"
-//    let a = Math.floor(Math.random()*3)
-//    console.log(a)
-//    if (a==0)
-//    {
-//     document.getElementById("com").style.background = "url('https://images.pexels.com/photos/161702/harmony-relax-rock-moqui-161702.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1') center/cover"
-//     document.getElementById("result").innerHTML="You Win"
-// }
-//     else if (a==1) {
-//         document.getElementById("com").style.background = "url('https://images.pexels.com/photos/268349/pexels-photo-268349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1https://cdn.pixabay.com/photo/2014/04/10/19/53/scissors-321238_960_720.jpg') center/cover"
-//         document.getElementById("result").innerHTML="Match Draw"
-//     }
-//     else if (a==2) {
-//         document.getElementById("com").style.background = "url('https://images.pexels.com/photos/4226910/pexels-photo-4226910.jpeg?auto=compress&cs=tinysrgb&w=1200') center/cover"
-//         document.getElementById("result").innerHTML="You Lose"
-
-//     }
-// }
-// function scissors() 
-// {
-//    document.getElementById("you").style.background = "url('https://images.pexels.com/photos/4226910/pexels-photo-4226910.jpeg?auto=compress&cs=tinysrgb&w=1200') center/cover"
-//    let a = Math.floor(Math.random()*3)
-//    console.log(a)
-//    if (a==0)
-//    {
-//     document.getElementById("com").style.background = "url('https://images.pexels.com/photos/161702/harmony-relax-rock-moqui-161702.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1') center/cover"
-//     document.getElementById("result").innerHTML="You Lose"
-// }
-//     else if (a==1) {
-//         document.getElementById("com").style.background = "url('https://images.pexels.com/photos/268349/pexels-photo-268349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1https://cdn.pixabay.com/photo/2014/04/10/19/53/scissors-321238_960_720.jpg') center/cover"
-//         document.getElementById("result").innerHTML="You Win"
-//     }
-//     else if (a==2) {
-//         document.getElementById("com").style.background = "url('https://images.pexels.com/photos/4226910/pexels-photo-4226910.jpeg?auto=compress&cs=tinysrgb&w=1200') center/cover"
-//         document.getElementById("result").innerHTML="Match Draw"
-//     }
-
-// }
 let player;
 let num = 10;
+let l=0;
+let w=0;
+let d=0;
 function rock()
 {
     num--;
@@ -73,7 +11,6 @@ function rock()
    player = 0;
    com();
    checkgameover();
-   
 }
 function paper()
 {
@@ -83,7 +20,6 @@ function paper()
    player = 1;
    com();
    checkgameover();
-
 }
 function scissors()
 {
@@ -109,57 +45,37 @@ let rand = Math.floor(Math.random()*3)
     {
         document.getElementById("com").style.background = "url('https://images.pexels.com/photos/4226910/pexels-photo-4226910.jpeg?auto=compress&cs=tinysrgb&w=1200') center/cover"
     }
-    if(player==0 && rand==0)
+    if((player==0 && rand==0)||(player==1 && rand==1)||(player==2 && rand==2))
     {
         document.getElementById("result").innerHTML = "Match Draw"
         document.getElementById("result").style.color = "white"
-    }
-    else if(player==0 && rand==1)
-    {
-        document.getElementById("result").innerHTML = "You lose"
-        document.getElementById("result").style.color = "red"
-    }
-    else if(player==0 && rand==2)
-    {
-        document.getElementById("result").innerHTML = "You Won"
-        document.getElementById("result").style.color = "green"
-    }
-    else if(player==1 && rand==0)
-    {
-        document.getElementById("result").innerHTML = "You Won"
-        document.getElementById("result").style.color = "green"
-    }
-    else if(player==1 && rand==1)
-    {
-        document.getElementById("result").innerHTML = "Match Draw"
-        document.getElementById("result").style.color = "white"
-    }
-    else if(player==1 && rand==2)
-    {
-        document.getElementById("result").innerHTML = "You lose"
-        document.getElementById("result").style.color = "red"
-    }
-    else if(player==2 && rand==0)
-    {
-        document.getElementById("result").innerHTML = "You lose"
-        document.getElementById("result").style.color = "red"
-    }
-    else if(player==2 && rand==1)
-    {
-        document.getElementById("result").innerHTML = "You Won"
-        document.getElementById("result").style.color = "green"
-    }
-    else if(player==2 && rand==2)
-    {
-        document.getElementById("result").innerHTML = "Match Draw"
-        document.getElementById("result").style.color = "white"
-    }
-}
-
-function checkgameover(){
-    if (num==-1) {
         
-        alert("Game Over")
+    }
+    else if((player==0 && rand==1)||(player==2 && rand==0)||(player==1 && rand==2))
+    {
+        document.getElementById("result").innerHTML = "You lose"
+        document.getElementById("result").style.color = "red"
+    }
+    else 
+    {
+        document.getElementById("result").innerHTML = "You Won"
+        document.getElementById("result").style.color = "green"
     }
 }
-
+function checkgameover(){
+    let f=document.getElementById("result").innerText
+    if (f=="Match Draw") {
+        d++;
+    }
+    else if (f=="You lose") {
+        l++;
+    }
+    else
+    {
+        w++
+    }
+    if (num==0) {
+        
+        alert(`Game Over win count${w}`)
+    }
+}
